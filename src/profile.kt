e5dbe30694fc65614e84a4f21752c487418c39d2
deshkin <59682797+deshkin@users.noreply.github.com>
2020-02-17 18:24:48 +0700

    Initial commit

---
+++ src/profile.kt
@@ -0,0 +1,8 @@
+class profile(){
+    var login: String? = null
+    var password: String? = null
+    var name: String? = null
+    var height: Double? = null
+    var weight: Double? = null
+    
+}
